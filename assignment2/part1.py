2eba94b7714a95452e48bf8991a021dbd12180e6
Toni Vucic <tonivuc@gmail.com>
2023-09-28 22:06:51 +0200

    Create user table

--- assignment2/part1.py
+++ assignment2/part1.py
@@ -1,13 +1,33 @@
 from SqlHelper import SqlHelper
 
+def create_user_table(sqlHelper):
+    query = """CREATE TABLE IF NOT EXISTS User (
+                id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                has_labels BOOLEAN)
+            """
+    # This adds table_name to the %s variable and executes the query
+    sqlHelper.cursor.execute(query)
+    sqlHelper.db_connection.commit()
+
+# def populate_users_table(self, table_name):
+#     query = """CREATE TABLE IF NOT EXISTS %s (
+#                 id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+#                 name VARCHAR(30))
+#             """
+#     # This adds table_name to the %s variable and executes the query
+#     self.cursor.execute(query % table_name)
+#     self.db_connection.commit()
+
 def main():
     sqlHelper = None
     try:
         sqlHelper = SqlHelper()
-        sqlHelper.create_table(table_name="Person")
-        sqlHelper.insert_data(table_name="Person", stringArray=['Bobby', 'Mc', 'McSmack', 'Board'])
-        _ = sqlHelper.fetch_data(table_name="Person")
-        sqlHelper.drop_table(table_name="Person")
+        create_user_table(sqlHelper)
+        sqlHelper.create_table(table_name="Activity")
+        sqlHelper.create_table(table_name="TrackPoint")
+        # sqlHelper.insert_data(table_name="Person", stringArray=['Bobby', 'Mc', 'McSmack', 'Board'])
+        _ = sqlHelper.fetch_data(table_name="User")
+        sqlHelper.drop_table(table_name="Activity")
         # Check that the table is dropped
         sqlHelper.show_tables()
     except Exception as e:
