f30a67332db372456a8fb6678c425f5af7ce0a8e
Toni Vucic <tonivuc@gmail.com>
2023-09-30 13:29:43 +0200

    Insert users

--- assignment2/part1.py
+++ assignment2/part1.py
@@ -4,7 +4,7 @@ import os
 # Kanskje ha id som STRING og ikke AUTO_INCREMENT
 def create_user_table(sqlHelper):
     query = """CREATE TABLE IF NOT EXISTS User ( 
-                id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                id VARCHAR(30) NOT NULL PRIMARY KEY,
                 has_labels BOOLEAN)
             """
     # This adds table_name to the %s variable and executes the query
@@ -41,45 +41,69 @@ def create_trackpoint_table(sqlHelper):
     sqlHelper.cursor.execute(query)
     sqlHelper.db_connection.commit()
 
-def populate_users_table(self, table_name):
-    query = """CREATE TABLE IF NOT EXISTS %s (
-                id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-                name VARCHAR(30))
-            """
+def populate_user_table(self, table_name):
+    labeled_user_ids_array = array_of_labeled_user_ids_from_file()
+    # Read the files and add them to the database
+
     # This adds table_name to the %s variable and executes the query
-    self.cursor.execute(query % table_name)
+    self.cursor.execute(query)
     self.db_connection.commit()
 
 def create_map_of_user_id_and_if_they_have_labels():
-    for (root,dirs,files) in os.walk('./dataset/dataset/Data', topdown=True):
-        print (root)
-        # print (dirs)
-        # print (files)
-        print ('--------------------------------')
+    # for (root,dirs,files) in os.walk('./dataset/dataset/Data', topdown=True):
+    #     print (root)
+    #     print (dirs)
+    #     # print (files)
+    #     print ('--------------------------------')
+    user_ids_with_labels = array_of_labeled_user_ids_from_file()
+    user_ids = []
+    data_dir = './dataset/dataset/Data'
+    for subdir in os.listdir(data_dir):
+        subdir_path = os.path.join(data_dir, subdir)
+        if os.path.isdir(subdir_path):
+            user_ids.append(subdir)
+    user_ids_and_if_they_have_labels = {id: id in user_ids_with_labels for id in user_ids}
 
-def array_of_user_ids_from_file():
+    # Print the dictionary
+    print(user_ids_and_if_they_have_labels)
+    return user_ids_and_if_they_have_labels
+
+def array_of_labeled_user_ids_from_file():
     # Open the file and read its contents into a list of strings
     with open('./dataset/dataset/labeled_ids.txt', 'r') as f:
         lines = f.readlines()
 
     # Strip any whitespace characters from each line and store them in a new list
     ids = [line.strip() for line in lines]
+    return ids
 
-    # Print the list of IDs
-    print(ids)
+def drop_all_tables(sqlHelper):
+    sqlHelper.drop_table("TrackPoint")
+    sqlHelper.drop_table("Activity")
+    sqlHelper.drop_table("User")
+
+def add_users_to_table(sqlHelper):
+    userMap = create_map_of_user_id_and_if_they_have_labels()
+    for id, has_labels in userMap.items():
+        sqlHelper.cursor.execute("INSERT INTO User (id, has_labels) VALUES (%s, %s)", (id, has_labels))
+
+    sqlHelper.db_connection.commit()
+
 
 def main():
     sqlHelper = None
     try:
-        array_of_user_ids_from_file()
-        # sqlHelper = SqlHelper()
+        # array_of_user_ids_from_file()
+        sqlHelper = SqlHelper()
+        
         # create_user_table(sqlHelper)
+        # add_users_to_table(sqlHelper)
+
         # create_activity_table(sqlHelper)
         # create_trackpoint_table(sqlHelper)
 
-
         # # sqlHelper.insert_data(table_name="Person", stringArray=['Bobby', 'Mc', 'McSmack', 'Board'])
-        # _ = sqlHelper.fetch_data(table_name="User")
+        _ = sqlHelper.fetch_data(table_name="User")
         # __ = sqlHelper.fetch_data(table_name="Activity")
         # ___ = sqlHelper.fetch_data(table_name="TrackPoint")
 
