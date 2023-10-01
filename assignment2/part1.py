ebc42b5ff178e2b81bc90274674ebeb8b873cc2e
Toni Vucic <tonivuc@gmail.com>
2023-10-01 20:39:20 +0200

    Insert activity for user 000

--- assignment2/part1.py
+++ assignment2/part1.py
@@ -1,5 +1,6 @@
 from SqlHelper import SqlHelper
 import os
+import traceback
 
 # Table creation
 def create_user_table(sqlHelper):
@@ -13,7 +14,7 @@ def create_user_table(sqlHelper):
 def create_activity_table(sqlHelper):
     query = """CREATE TABLE IF NOT EXISTS Activity (
                 id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-                user_id INT,
+                user_id VARCHAR(30),
                 transportation_mode VARCHAR(30),
                 start_date_time DATETIME,
                 end_date_time DATETIME,
@@ -80,6 +81,71 @@ def add_users_to_table(sqlHelper):
     sqlHelper.db_connection.commit()
 
 # Activity stuff
+# One activity consits of many trackpoints (each trackpoint refers to an activity, but activities are freestanding entries with a start and end time)
+# Can probably insert the activities in one go, and then add the transportation modes later?
+# Just make sure to exclude .plt files with more than 2500 lines (+ 6 lines of the top metadata)
+# When adding the transportation mode, we have to check that the activity start time (in the mySQL row) matches the start-time in the labels.txt
+def read_trackpoints(user_id):
+    # Define the path to the Trajectory directory
+    path = os.path.join("dataset", "dataset", "Data", user_id, "Trajectory")
+    # Initialize an empty list to store the objects
+    filename_and_trackpoints = {}
+    
+    # Loop over the .plt files in the Trajectory directory
+    for filename in os.listdir(path):
+        if filename.endswith(".plt"):
+            filepath = os.path.join(path, filename)
+
+            trackpoints = []
+            
+            # Open the file and skip the first 6 lines
+            with open(filepath, "r") as f:
+                for i in range(6):
+                    next(f)
+                
+                # Read the remaining lines into objects
+                for line in f:
+                    if len(trackpoints) >= 2500:
+                        break
+                    values = line.strip().split(",")
+                    obj = {
+                        "latitude": float(values[0]),
+                        "longitude": float(values[1]),
+                        "altitude": float(values[2]),
+                        "date": values[5],
+                        "time": values[6]
+                    }
+                    trackpoints.append(obj)
+
+        filename_and_trackpoints[filename] = trackpoints
+    
+    return filename_and_trackpoints
+
+def create_activities_from_trackpoints(filename_and_trackpoints, sqlHelper: SqlHelper, user_id):
+    for filename, trackpoints in filename_and_trackpoints.items():
+        # Get the start and end date/time from the trackpoints
+        start_date_time = trackpoints[0]["date"] + " " + trackpoints[0]["time"]
+        end_date_time = trackpoints[-1]["date"] + " " + trackpoints[-1]["time"]
+        
+        # Insert the activity into the database
+        sql = "INSERT INTO Activity (user_id, start_date_time, end_date_time) VALUES (%s, %s, %s)"
+        values = (user_id, start_date_time, end_date_time)
+        activity_id = sqlHelper.cursor.execute(sql, values)
+        print(activity_id)
+
+        # insert_query = """INSERT INTO Activity (user_id, start_date_time, end_date_time) VALUES ({user_id}, {start_date_time}, {end_date_time})"""
+
+        
+        
+        
+        # Insert the trackpoints into the database
+        # for trackpoint in trackpoints:
+        #     sql = "INSERT INTO TrackPoint (activity_id, lat, lon, altitude, date_days, date_time) VALUES (?, ?, ?, ?, ?, ?)"
+        #     values = (activity_id, trackpoint["latitude"], trackpoint["longitude"], trackpoint["altitude"], None, trackpoint["date"] + " " + trackpoint["time"])
+        #     sqlHelper.cursor.execute(sql, values)
+
+        # Commit the changes to the database
+        sqlHelper.db_connection.commit()
 
 # Trackpoint stuff
 
@@ -88,22 +154,29 @@ def main():
     try:
         # array_of_user_ids_from_file()
         sqlHelper = SqlHelper()
+        # drop_all_tables(sqlHelper)
         
         # create_user_table(sqlHelper)
         # add_users_to_table(sqlHelper)
-
         # create_activity_table(sqlHelper)
         # create_trackpoint_table(sqlHelper)
 
+        filename_and_trackpoints = read_trackpoints("000")
+        create_activities_from_trackpoints(filename_and_trackpoints, sqlHelper, "000")
+        # filename, trackpoints = next(iter(activities.items()))
+        # print(trackpoints)
+        # print(filename)
+
         # # sqlHelper.insert_data(table_name="Person", stringArray=['Bobby', 'Mc', 'McSmack', 'Board'])
-        _ = sqlHelper.fetch_data(table_name="User")
-        # __ = sqlHelper.fetch_data(table_name="Activity")
+        # _ = sqlHelper.fetch_data(table_name="User")
+        __ = sqlHelper.fetch_data(table_name="Activity")
         # ___ = sqlHelper.fetch_data(table_name="TrackPoint")
 
         # # Check that the table is dropped
         # sqlHelper.show_tables()
     except Exception as e:
         print("ERROR: Failed to use database:", e)
+        traceback.print_exc()
     finally:
         if sqlHelper:
             sqlHelper.connection.close_connection()
