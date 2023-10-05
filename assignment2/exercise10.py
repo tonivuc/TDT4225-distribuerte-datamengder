cef84458f7976026cdd713d27a73ab9058001998
Toni Vucic <tonivuc@gmail.com>
2023-10-05 23:05:05 +0200

    Sum up activities for each day, user 128 omitted

--- assignment2/exercise10.py
+++ assignment2/exercise10.py
@@ -39,12 +39,22 @@ def find_longest_distances_traveled_per_transportation_mode_for_user(sqlHelper, 
         if (activity[0] not in transportation_modes_dict):
             transportation_modes_dict[activity[0]] = 0
 
+    total_distance_for_date = {}
     for activity in user_activities_with_transportation_mode:
         activity_id = activity[2]
         activity_start_datetime = activity[3]
-        distance = get_distances_between_trackpoints(sqlHelper, activity_id, activity_start_datetime)
-        if (distance > transportation_modes_dict[activity[0]]):
-            transportation_modes_dict[activity[0]] = distance
+        # Get the start of day daytime for activity_start_datetime
+        day_start_time = activity_start_datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+        distance = get_distances_between_trackpoints(sqlHelper, activity_id, day_start_time)
+        # If total_distance_for_date[day_start_time] is not a number yet, set it to 0
+        if (day_start_time not in total_distance_for_date):
+            total_distance_for_date[day_start_time] = 0
+        total_distance_for_date[day_start_time] = total_distance_for_date[day_start_time] + distance
+        # print("Total distance traveled for this day: {}. With transportation mode {}".format(total_distance_for_date[day_start_time], activity[0]))
+        
+        # If we found a date with a longer total distance than the one we already have stored, update the dictionary
+        if (total_distance_for_date[day_start_time] > transportation_modes_dict[activity[0]]):
+            transportation_modes_dict[activity[0]] = total_distance_for_date[day_start_time]
         
     return transportation_modes_dict
 
@@ -67,7 +77,8 @@ def find_distance_traveled_transportation_mode_all_users(sqlHelper):
         user_id = ''    # Default value
         transport_mode_distance_and_user_id[transport_mode] = [distance, user_id]
 
-    relevant_user_ids = get_user_ids_with_labeled_activities(sqlHelper)
+    relevant_user_ids_including_128 = get_user_ids_with_labeled_activities(sqlHelper)
+    relevant_user_ids = [user_id for user_id in relevant_user_ids_including_128 if user_id != '128']
     for user_id in relevant_user_ids:
         transport_modes_and_distance = find_longest_distances_traveled_per_transportation_mode_for_user(sqlHelper, user_id)
         for transport_mode, distance in transport_modes_and_distance.items():
@@ -79,18 +90,18 @@ def find_distance_traveled_transportation_mode_all_users(sqlHelper):
     print(transport_mode_distance_and_user_id)
 
 
-def get_distances_between_trackpoints(sqlHelper, activity_id, activity_start_datetime):
+def get_distances_between_trackpoints(sqlHelper, activity_id, start_datetime):
     # activity_start_datetime = datetime.strptime(activity_start_datetime, "%Y-%m-%d %H:%M:%S") # Use this if input is string
-    activity_start_plus_24h = activity_start_datetime + timedelta(hours=24)
-
+    activity_start_plus_24h = start_datetime + timedelta(hours=24)
     # The query calculates the total distance traveled between consecutive trackpoints 
     # for a specified activity and within a specified date and time range (24h from activity start)
 
     # The subquery gets us the next trackpoint. And the parent query calculates the distance between them.
     # Finally the parent of that one sums up all the distances in each row, giving us the total distance traveled.
+    # We also round the distance as GPS isn't accurate anyway and it's nicer to display that way. Also we won't do any more calculations with it at this point.
     query = """
     SELECT 
-        SUM(distance_to_next_trackpoint) AS total_distance
+        ROUND(SUM(distance_to_next_trackpoint)) AS total_distance
     FROM (
         SELECT 
             tp.id AS trackpoint_id,
@@ -111,9 +122,11 @@ def get_distances_between_trackpoints(sqlHelper, activity_id, activity_start_dat
         ) AS tp
         JOIN TrackPoint tp1 ON tp.next_id = tp1.id
     ) AS distances;
-    """.format(activity_id, activity_start_datetime, activity_start_plus_24h)
+    """.format(activity_id, start_datetime, activity_start_plus_24h)
     rows = fetchData(sqlHelper, query)
     distance = rows[0][0]
+    if (distance == None):
+        distance = 0
     return distance
 
 def main():
